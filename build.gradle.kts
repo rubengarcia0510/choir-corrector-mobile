5605d91ee9d13f02d17fdc8f6c71d531feefd571
Ruben Garcia <rubengarcia0510@gmail.com>
2026-09-09 19:45:58 -0300

    feat: initialize Choir Corrector mobile app

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("org.jetbrains.kotlin.multiplatform") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("com.android.application") version "8.13.1" apply false
+    id("org.jetbrains.compose") version "1.9.3" apply false
+}
---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.android.application")
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    androidTarget()
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation("androidx.activity:activity-compose:1.10.1")
+        }
+
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
+
+android {
+    namespace = "com.rubengarcia.choircorrector"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.rubengarcia.choircorrector"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
