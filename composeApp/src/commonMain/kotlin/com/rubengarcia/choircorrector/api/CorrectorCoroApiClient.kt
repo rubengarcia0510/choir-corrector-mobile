7b28cdf71916380458e94f7e34f9211acb516274
rubengarcia0510 <rubengarcia0510@gmail.com>
2026-09-13 20:46:57 -0300

    Fix Ktor imports and add missing API methods

--- composeApp/src/commonMain/kotlin/com/rubengarcia/choircorrector/api/CorrectorCoroApiClient.kt
+++ composeApp/src/commonMain/kotlin/com/rubengarcia/choircorrector/api/CorrectorCoroApiClient.kt
@@ -1,21 +1,72 @@
 package com.rubengarcia.choircorrector.api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.request.forms.MultiPartFormDataContent
+import io.ktor.client.request.forms.formData
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
 import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
-import io.ktor.http.content.MultiPartFormDataContent
-import io.ktor.http.content.PartData
-import io.ktor.http.content.formData
 import java.io.File
 
 class CorrectorCoroApiClient(
     private val baseUrl: String,
     private val httpClient: HttpClient = HttpClient()
 ) {
+    
+    suspend fun uploadReference(
+        referenceAudioFile: File
+    ): HttpResponse {
+        val referenceBytes = referenceAudioFile.readBytes()
+        
+        return httpClient.post("$baseUrl/api/upload/reference") {
+            setBody(
+                MultiPartFormDataContent(
+                    formData {
+                        append(
+                            key = "reference_audio",
+                            value = referenceBytes,
+                            headers = Headers.build {
+                                append(
+                                    HttpHeaders.ContentDisposition,
+                                    "form-data; name=\"reference_audio\"; filename=\"${referenceAudioFile.name}\""
+                                )
+                                append(HttpHeaders.ContentType, ContentType.Audio.WAV.toString())
+                            }
+                        )
+                    }
+                )
+            )
+        }
+    }
+    
+    suspend fun uploadRehearsal(
+        rehearsalAudioFile: File
+    ): HttpResponse {
+        val rehearsalBytes = rehearsalAudioFile.readBytes()
+        
+        return httpClient.post("$baseUrl/api/upload/rehearsal") {
+            setBody(
+                MultiPartFormDataContent(
+                    formData {
+                        append(
+                            key = "rehearsal_audio",
+                            value = rehearsalBytes,
+                            headers = Headers.build {
+                                append(
+                                    HttpHeaders.ContentDisposition,
+                                    "form-data; name=\"rehearsal_audio\"; filename=\"${rehearsalAudioFile.name}\""
+                                )
+                                append(HttpHeaders.ContentType, ContentType.Audio.WAV.toString())
+                            }
+                        )
+                    }
+                )
+            )
+        }
+    }
     
     suspend fun uploadAudioFiles(
         referenceAudioFile: File,
@@ -36,7 +87,7 @@ class CorrectorCoroApiClient(
                                     HttpHeaders.ContentDisposition,
                                     "form-data; name=\"reference_audio\"; filename=\"${referenceAudioFile.name}\""
                                 )
-                                append(HttpHeaders.ContentType, ContentType.Audio.WAV)
+                                append(HttpHeaders.ContentType, ContentType.Audio.WAV.toString())
                             }
                         )
                         append(
@@ -47,7 +98,7 @@ class CorrectorCoroApiClient(
                                     HttpHeaders.ContentDisposition,
                                     "form-data; name=\"rehearsal_audio\"; filename=\"${rehearsalAudioFile.name}\""
                                 )
-                                append(HttpHeaders.ContentType, ContentType.Audio.WAV)
+                                append(HttpHeaders.ContentType, ContentType.Audio.WAV.toString())
                             }
                         )
                     }
@@ -74,7 +125,7 @@ class CorrectorCoroApiClient(
                                     HttpHeaders.ContentDisposition,
                                     "form-data; name=\"reference_audio\"; filename=\"$referenceFileName\""
                                 )
-                                append(HttpHeaders.ContentType, ContentType.Audio.WAV)
+                                append(HttpHeaders.ContentType, ContentType.Audio.WAV.toString())
                             }
                         )
                         append(
@@ -85,12 +136,28 @@ class CorrectorCoroApiClient(
                                     HttpHeaders.ContentDisposition,
                                     "form-data; name=\"rehearsal_audio\"; filename=\"$rehearsalFileName\""
                                 )
-                                append(HttpHeaders.ContentType, ContentType.Audio.WAV)
+                                append(HttpHeaders.ContentType, ContentType.Audio.WAV.toString())
                             }
                         )
                     }
                 )
             )
+        }
+    }
+    
+    suspend fun getStatus(
+        analysisId: String
+    ): HttpResponse {
+        return httpClient.post("$baseUrl/api/status/$analysisId") {
+            // Empty body for GET-like status check
+        }
+    }
+    
+    suspend fun getResult(
+        analysisId: String
+    ): HttpResponse {
+        return httpClient.post("$baseUrl/api/result/$analysisId") {
+            // Empty body for GET-like result retrieval
         }
     }
 }
