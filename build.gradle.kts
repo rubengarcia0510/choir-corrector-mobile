a8b69bf22e84e3727e9ed2574301632297725751
Ruben Garcia <rubengarcia0510@gmail.com>
2026-09-13 22:18:37 -0300

    fix Kotlin serialization plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.jetbrains.kotlin.multiplatform") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("com.android.application") version "8.13.1" apply false
     id("org.jetbrains.compose") version "1.9.3" apply false
 }
