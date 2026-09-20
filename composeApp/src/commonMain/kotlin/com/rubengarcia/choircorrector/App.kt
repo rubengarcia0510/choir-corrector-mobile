dd6a07401d29cd4b1896c3a56ac6babb0d796622
Ruben Garcia <rubengarcia0510@gmail.com>
2026-09-20 15:17:55 -0300

    fix: disable analysis for free users

--- composeApp/src/commonMain/kotlin/com/rubengarcia/choircorrector/App.kt
+++ composeApp/src/commonMain/kotlin/com/rubengarcia/choircorrector/App.kt
@@ -127,7 +127,7 @@ private fun HomeScreen(
 
         Button(
             onClick = onNewAnalysis,
-            enabled = isPro != null
+            enabled = isPro == true
         ) {
             Text("New analysis")
         }
